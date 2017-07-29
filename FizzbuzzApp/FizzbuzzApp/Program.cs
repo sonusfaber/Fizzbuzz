f3170a4ea5b1c483b2485225211b7713b0715ed1
trey causey <tcausey808@gmail.com>
2017-07-29 10:44:38 -0400

    Fizzbuzz Beta 0.1

---
+++ FizzbuzzApp/FizzbuzzApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzbuzzApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 100; i++)
+            {
+                if ((i % 3) == 0)
+                {
+                    Console.Write("fizz");
+                }
+
+                if ((i % 5) == 0)
+                {
+
+                    Console.WriteLine("buzz");
+
+                }
+                else
+                {
+                    Console.WriteLine(i.ToString());
+                }
+
+            }
+
+        }
+    }
+}
